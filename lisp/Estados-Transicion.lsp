5229211b0432ded9c47fe807b6ca4294c7532e22
Lau.dev.jr <britezlau.dev@gmail.com>
2026-06-09 20:53:52 -0300

    Arreglo de la transicion de colores por segunda vez

--- lisp/Estados-Transicion.lsp
+++ lisp/Estados-Transicion.lsp
@@ -6,7 +6,7 @@
 ;; ========================================================
 (defun transicion (color-actual cambiar-a)
   (cond
-     ((and (equal cambiar-a 'verde) (equal color-actual 'en-rojo)) (list color-actual "cambiar-a-verde")) 
-     ((and (equal cambiar-a 'amarillo) (equal color-actual 'en-verde)) (list color-actual "cambiar-a-amarillo")) 
-     ((and (equal cambiar-a 'rojo) (equal color-actual 'en-amarillo)) (list color-actual "cambiar-a-rojo"))
+     ((and (equal cambiar-a 'rojo) (equal color-actual 'en-verde)) (list color-actual "cambiar-a-rojo")) 
+     ((and (equal cambiar-a 'amarillo) (equal color-actual 'en-rojo)) (list color-actual "cambiar-a-amarillo")) 
+     ((and (equal cambiar-a 'verde) (equal color-actual 'en-amarillo)) (list color-actual "cambiar-a-verde"))
      (t (list color-actual "accion-por-defecto"))))
--- lisp/core.lisp
+++ lisp/core.lisp
@@ -6,16 +6,17 @@
 ;; ========================================================
 (defun transicion (color-actual cambiar-a)
   (cond
-     ((and (equal cambiar-a 'verde) (equal color-actual 'en-rojo)) (list color-actual "cambiar-a-verde")) 
-     ((and (equal cambiar-a 'amarillo) (equal color-actual 'en-verde)) (list color-actual "cambiar-a-amarillo")) 
-     ((and (equal cambiar-a 'rojo) (equal color-actual 'en-amarillo)) (list color-actual "cambiar-a-rojo"))
+     ((and (equal cambiar-a 'rojo) (equal color-actual 'en-verde)) (list color-actual "cambiar-a-rojo")) 
+     ((and (equal cambiar-a 'amarillo) (equal color-actual 'en-rojo)) (list color-actual "cambiar-a-amarillo")) 
+     ((and (equal cambiar-a 'verde) (equal color-actual 'en-amarillo)) (list color-actual "cambiar-a-verde"))
      (t (list color-actual "accion-por-defecto"))))
 ;; ========================================================
-;; Ejemplo de uso normal: (transicion 'en-rojo 'verde) --> (EN-ROJO "cambiar-a-verde")
-;;                        (transicion 'en-verde 'amarillo) --> (EN-VERDE "cambiar-a-amarillo")
-;;                        (transicion 'en-amarillo 'rojo) --> (EN-AMARILLO "cambiar-a-rojo")
-;; Ejemplo de casos de error: (transicion 'en-rojo 'amarillo) --> (EN-ROJO "accion-por-defecto")
-;;                            (transicion 'en-verde 'rojo) --> (EN-VERDE "accion-por-defecto")
+;; Ejemplo de uso normal: (transicion 'en-verde 'rojo) --> (EN-VERDE "cambiar-a-rojo")
+;;                        (transicion 'en-rojo 'amarillo) --> (EN-ROJO "cambiar-a-amarillo")
+;;                        (transicion 'en-amarillo 'verde) --> (EN-AMARILLO "cambiar-a-verde")
+;; Ejemplo de casos de error: (transicion 'en-rojo 'verde) --> (EN-ROJO "accion-por-defecto")
+;;                            (transicion 'en-verde 'amarillo) --> (EN-VERDE "accion-por-defecto")
+;;                            (transicion 'en-amarillo 'rojo) --> (EN-AMARILLO "accion-por-defecto")
 ;;                            (transicion 'en-rojo 'azul) --> (EN-ROJO "accion-por-defecto")
 ;; ========================================================
 

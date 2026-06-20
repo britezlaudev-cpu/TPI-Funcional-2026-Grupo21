1df331a1f4362bba13abf9eb93e2c480fc59f8f1
Lau.dev.jr <britezlau.dev@gmail.com>
2026-06-20 02:12:46 -0300

    actualizacion/arreglo funcion transicion

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,4 +1,4 @@
-;requerimiento 1
+;; requerimiento 1
 ;; ========================================================
 ;; FUNCION: transicion
 ;; NATURALEZA: Pura (Debido a LIST, siempre retornara una lista limpia, dejando los datos originales intactos)
@@ -8,13 +8,15 @@
 
 (defun transicion (color-actual cambiar-a) ; recibe dos parametros, el color actual del semaforo y el color al que se cambie
   (cond                                    ; una estructura condiconal para evaluar las transiciones validas entre colores
-     ((and (equal cambiar-a 'rojo) (equal color-actual 'en-verde)) (list color-actual "cambiar-a-rojo")) ; and para evaluar ambas condiciones y equal para comparar los simbolos
-     ((and (equal cambiar-a 'amarillo) (equal color-actual 'en-rojo)) (list color-actual "cambiar-a-amarillo")) ; de ser verdadero retorna una lista con el color actual y la accion a realizar
-     ((and (equal cambiar-a 'verde) (equal color-actual 'en-amarillo)) (list color-actual "cambiar-a-verde"))
-     ((and (equal cambiar-a 'rojo-intermitente) (equal color-actual 'en-verde)) (list color-actual "rojo-intermitente")) 
+     ((and (equal cambiar-a 'verde) (equal color-actual 'en-rojo)) (list color-actual "cambiar-a-verde")) ; and para evaluar ambas condiciones y equal para comparar los simbolos
+     ((and (equal cambiar-a 'amarillo) (equal color-actual 'en-verde)) (list color-actual "cambiar-a-amarillo")) ; de ser verdadero retorna una lista con el color actual y la accion a realizar
+     ((and (equal cambiar-a 'rojo) (equal color-actual 'en-amarillo)) (list color-actual "cambiar-a-rojo"))
+     ; para complemnetra se actualizo la funcion transicion para incluir los requerimientos de la iteracion2|extension1
+     ((and (equal cambiar-a 'rojo-intermitente) (equal color-actual 'en-verde)) (list color-actual "rojo-intermitente")) ; esto funcionaria mejor con la ayuda del timer
      ((and (equal cambiar-a 'amarillo-intermitente) (equal color-actual 'en-rojo)) (list color-actual "amarillo-intermitente")) 
      ((and (equal cambiar-a 'verde-intermitente) (equal color-actual 'en-amarillo)) (list color-actual "verde-intermitente"))
-     (t (list color-actual "accion-por-defecto")))) ; si todas las condicones son falsas, retorna una accion por defecto
+     (t (list color-actual "accion-por-defecto")))) ; si todas las condicones son falsas, retorna una accion por defecto, claro que tambien
+                                                    ; retornaria un color intermediario de ser necesario 
 
 ;requerimiento 2
 ;;=========================================================
